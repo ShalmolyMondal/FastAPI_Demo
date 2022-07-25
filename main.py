00641818ee2ce141a80921a48e1d2483d8d41239
ShalmolyMondal <70585546+ShalmolyMondal@users.noreply.github.com>
2022-07-25 15:40:23 +1000

    commit all

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+import numpy as np
+import json
+import time
+import skfuzzy as skf
+
+
+app = FastAPI()  # creating API object
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.websocket("/websocket")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = json.loads(await websocket.receive_text())
+        speed_data = data["speed"]
+        density_data = data["density"]
+        rand_speed = list(
+            np.random.randint(speed_data["lower"], speed_data["upper"], size=1)
+        )[0]
+        rand_density = list(
+            np.random.randint(density_data["lower"], density_data["upper"], size=1)
+        )[0]
+
+        membership_speed = skf.trapmf(np.array([rand_speed]), [18, 20, 35, 40])
+        membership_density = skf.trapmf(np.array([rand_density]), [18, 20, 35, 40])
+
+        certainity = list(membership_speed)[0] * 0.5 + list(membership_density)[0] * 0.5
+        timestamp = time.time()
+        result = {
+            "Speed": rand_speed,
+            "Density": rand_density,
+            "timestamp": timestamp,
+            "certainity": certainity,
+        }
+        await websocket.send_text(f"{result}")
